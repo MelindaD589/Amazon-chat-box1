a8bb4a3db37b7e3b56970d8f8a905df039ea6e9c
Melinda Dothan <melinda.dothan@gmail.com>
2021-03-08 10:15:43 +0100

    Amazon chatbox in Python

---
+++ amazon chat box1.py
@@ -0,0 +1,21 @@
+# I opened Amazon's Customer Service on the app, then I chose Contact Us. Then the chat box starts like this. The plan is to write my code after what I see.
+print("Hello! It's Amazon's Chat Assistant again.")
+print("So, what can I help you with?")
+
+# Here you can choose from four options
+
+help list = input("An item I ordered", "Managing my payment, Prime, or account", "Help with Kindle, Fire, or Alexa device", "Music, eBooks, Prime Video, etc.")
+
+# Ok, I choose the first
+"An item I ordered"
+
+# A list of my previous orders come up. 
+if answer == "An item I ordered":
+    print("Select the item that you need help with.")
+
+# I do not choose any of them.
+"Item not on list." 
+
+# Messaging Assistant, Customer Service
+if answer == "Item not on list"
+    print("I'm sorry to hear that. Let me know which of these you need help with.")
