41f226436e3f39fb3f8f762be95ab74957820083
Melinda Dothan <melinda.dothan@gmail.com>
2021-03-08 10:34:43 +0100

    Fixing the code

--- amazon chat box1.py
+++ amazon chat box1.py
@@ -1,4 +1,5 @@
 # I opened Amazon's Customer Service on the app, then I chose Contact Us. Then the chat box starts like this. The plan is to write my code after what I see.
+
 print("Hello! It's Amazon's Chat Assistant again.")
 print("So, what can I help you with?")
 
@@ -7,15 +8,15 @@ print("So, what can I help you with?")
 help list = input("An item I ordered", "Managing my payment, Prime, or account", "Help with Kindle, Fire, or Alexa device", "Music, eBooks, Prime Video, etc.")
 
 # Ok, I choose the first
-"An item I ordered"
+answer1 = "An item I ordered"
 
 # A list of my previous orders come up. 
-if answer == "An item I ordered":
+if answer1 == "An item I ordered":
     print("Select the item that you need help with.")
 
 # I do not choose any of them.
-"Item not on list." 
+answer2 = "Item not on list." 
 
 # Messaging Assistant, Customer Service
-if answer == "Item not on list"
+if answer2 == "Item not on list"
     print("I'm sorry to hear that. Let me know which of these you need help with.")
